1ca8b077d92b864909344b5f470abdfc8a5c7ae2
RAHUL Danu <56379013+rahul-art@users.noreply.github.com>
2021-10-19 01:37:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -132,8 +132,8 @@ def mediapipe_webcam(imag):
             if cv2.waitKey(1) & 0xFF == ord('q'): 
                 break
         
-    cap.release()
-    cv2.destroyAllWindows()
+    #cap.release()
+    #cv2.destroyAllWindows()
     return img
 
 try:
